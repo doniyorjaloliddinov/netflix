aae843e2de323e955a8d241d347ecc4efeda52c0
doniyorjonjaloliddinov <don2003.01.09@gmail.com>
2022-05-27 21:04:24 +0500

    add filters for searching

--- js/script.js
+++ js/script.js
@@ -1,69 +1,120 @@
-// VARIABLES
+// FORM VARIABLES
 let elFormMovie = $(".js-movie-form");
 let elSearchInput = $(".js-search-input", elFormMovie);
-let elReadyList = $(".js-movie-list");
-let elTemplateMovie = $("#movie-template").content;
-// console.log(elFormMovie,elSearchInput, elReadyList, elTemplateMovie);
-movies.splice(100);
+let elCategoriesSelection = $(".js-categories-selection", elFormMovie);
+let elRatingSelection = $(".js-moives-rating", elFormMovie);
+let elAlphabetSelection = $(".js-alphabet-select", elFormMovie);
+let elContentsTemplate = $("#movie-template").content;
+let elReadyList = $(".js-movie-list",);
+let elReadyMoviesBtn = $(".js-search-btn");
+// console.log(elFormMovie,elSearchInput,elCategoriesSelection,elRatingSelection,elAlphabetSelection);
 
-// MAKING MOVIE ARRAY MORE CLEAR
+
+// NORMALIZED MOVIES ARRAY IN OBJECT
 let normalizedMovies = movies.map((movie, i) => {
-    return movie = {
+    return {
         id: i + 1,
-        image: `http://i3.ytimg.com/vi/${movie.ytid}/hqdefault.jpg`,
-        title: movie.Title,
+        title: movie.Title.toString(),
         year: movie.movie_year,
-        info: movie.summary,
-        video: movie.ytid
+        categories: movie.Categories.split("|").join(", "),
+        rating: movie.imdb_rating,
+        image: `http://i3.ytimg.com/vi/${movie.ytid}/hqdefault.jpg`,
+        video: `https://www.youtube.com/watch?v=${movie.ytid}`,
     }
+
 })
+normalizedMovies.splice(50);
 // console.log(normalizedMovies);
 
-// CREATING READY ELEMENTS
-let creatReadyElements = (movie) => {
-elReadyList.innerHTML = "";
 
-    let readyElements = elTemplateMovie.cloneNode(true);
-
-    $(".js-item-img", readyElements).src = movie.image;
-    $(".js-item-img", readyElements).alt = movie.title;
-    $(".js-heading-item", readyElements).textContent = movie.title;
-    $(".movie-primier", readyElements).textContent = movie.year;
-    $(".js-movie-link", readyElements).href = `https://www.youtube.com/watch?v=${movie.video}`;
-    $(".js-movie-link", readyElements).target = "_blank";
-
-    return readyElements;
-}
-
-
-// CREATING FUNCTION FRAGMENT
-let renderMovies = (movies) => {
+// READY CONTENT ELEMENTS AND MAKING FRAGMENT FUNC
+let renderedMovies = () => {
+    elReadyList.innerHTML = " ";
     let elResultFragment = document.createDocumentFragment();
 
-    movies.forEach((movie) => {
-        elResultFragment.appendChild(creatReadyElements(movie));
+    normalizedMovies.forEach((movie) => {
+        let readyList = elContentsTemplate.cloneNode(true);
+
+        $(".js-item-img", readyList).src = movie.image;
+        $(".js-heading-item", readyList).textContent = movie.title;
+        $(".movie-primier", readyList).textContent = movie.year;
+        $(".js-movie-link", readyList).href = movie.video;
+        $(".js-rating-movie", readyList).textContent = movie.rating;
+
+        elResultFragment.appendChild(readyList);
     })
-
     elReadyList.appendChild(elResultFragment);
-}
+};
+renderedMovies();``
 
-renderMovies(normalizedMovies);
-// console.log(renderMovies(normalizedMovies));
-
-// SEARCH INPPUT FUNCTION
-// debugger;
-elFormMovie.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    let searchMovies = new RegExp(elSearchInput.value.trim(), "gi");
-
-    let searchResult = normalizedMovies.filter((movie) => {
-        if(movie.title.match(searchMovies)) {
-            return movie.title.match(searchMovies);
-            console.log("smth ");
+let sortObjectsAZ = function (array) {
+    return array.sort(function (a, b) {
+        if (a.title > b.title) {
+            return 1;
+        } else if (a.title < b.title) {
+            return -1;
+        } else {
+            return 0;
         }
     })
-    renderMovies(searchResult);
+}
 
-    console.log(renderMovies(searchResult));
+
+let sortSearchResults = function (results, sortType) {
+    if (sortType === "az") {
+        sortObjectsAZ(results);
+    } else if (sortType === "za") {
+        sortObjectsAZ(results).reverse();
+    }
+}
+
+let findMovies = function (title, ratings) {
+    return normalizedMovies.filter(function (movie) {
+        return movie.title.match(title) && movie.rating >= ratings;
+    });
+}
+
+
+// GENRES SELECTION FUNC 
+let genresSelectionOption = () => {
+    let movieCategories = [];
+
+
+    normalizedMovies.forEach((movie) => {
+        movie.categories.split(" ").forEach((category) => {
+            if (!movieCategories.includes(category)) {
+                movieCategories.push(category)
+            }
+        })
+        movieCategories.sort();
+    })
+
+    let optionFragment = document.createDocumentFragment();
+
+    movieCategories.forEach((category) => {
+        let categoryOption = $$("option", category, "categoryOptions");
+
+        optionFragment.append(categoryOption);
+        elCategoriesSelection.append(categoryOption)
+    })
+
+}
+genresSelectionOption();
+
+
+// FORM FILTER FUNC 
+elReadyMoviesBtn.addEventListener("change", (evt) => {
+    evt.preventDefault();
+
+    let searchResult = elSearchInput.value.trim();
+    let searchMovies = new RegExp(searchResult, "gi");
+
+    let minimumRating = Number(elRatingSelection.value);
+    let sorting = elAlphabetSelection.value;
+
+    let searchResults = findMovies(searchMovies, minimumRating);
+    sortSearchResults(searchResults,sorting);
+
+    // console.log(minimumRating, sorting);
+    renderedMovies(searchResults);
 });
